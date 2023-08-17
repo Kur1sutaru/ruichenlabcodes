a71cd5eb476bc07676853cbdb41f820cfa4f336b
Cristal Villalba <52304532+Kur1sutaru@users.noreply.github.com>
2023-08-17 15:23:35 -0500

    Add files via upload

---
+++ deployshinycellapp.R
@@ -0,0 +1,28 @@
+setwd()
+#Install shiny cell
+reqPkg = c("data.table", "Matrix", "hdf5r", "reticulate", "ggplot2", 
+           "gridExtra", "glue", "readr", "RColorBrewer", "R.utils", "Seurat")
+newPkg = reqPkg[!(reqPkg %in% installed.packages()[,"Package"])]
+if(length(newPkg)){install.packages(newPkg)}
+
+# If you are using h5ad file as input, run the code below as well
+# reticulate::py_install("anndata")
+
+#To install shiny cell dependencies
+reqPkg = c("shiny", "shinyhelper", "data.table", "Matrix", "DT", "hdf5r", 
+           "reticulate", "ggplot2", "gridExtra", "magrittr", "ggdendro")
+newPkg = reqPkg[!(reqPkg %in% installed.packages()[,"Package"])]
+if(length(newPkg)){install.packages(newPkg)}
+
+# To install shiny cell
+devtools::install_github("SGDDNB/ShinyCell")
+
+
+# To launch rds file
+library(Seurat)
+library(ShinyCell)
+
+seu = readRDS("D:/baylor/TRIGEMINAL/b6tgnuintegrateddim20res01annotated.rds")
+scConf = createConfig(seu)
+makeShinyApp(seu, scConf, gene.mapping = TRUE,
+             shiny.title = "TG Quick Start") 
